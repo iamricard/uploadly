3ca8af9ded8e099dd5e230d497fee2a267b2e207
Ricard Sole <ricardsolecasas@gmail.com>
2017-02-11 18:11:00 +0100

    uses gulp to prepare for future multiple bundles

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const envify = require('envify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
+const id = (x) => x
+const makeBundler = (path, wrapper = id, opts = {}) => {
+  const options = Object.assign({}, opts, {
+    entries: `./extension/lib/${path}.js`
+  });
+  const bundler = wrapper(browserify(options));
+
+  const bundle = () =>
+    bundler
+      .bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source(`${path}-bundle.js`))
+      .pipe(buffer())
+      .pipe(gulp.dest('./extension/dist'));
+
+  bundler.transform(envify);
+  bundler.on('update', bundle);
+  bundler.on('log', gutil.log);
+
+  return bundle;
+};
+
+const watch = path => makeBundler(path, watchify, watchify.args)
+
+gulp.task('watch:background', watch('background'));
+gulp.task('compile:background', makeBundler('background'))
+gulp.task('compile', ['compile:background']);
+gulp.task('default', ['watch:background']);
