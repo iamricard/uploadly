ee98d2c1c87822abefbf6f6df9022dd1d61fc027
Ricard Sole <ricardsolecasas@gmail.com>
2017-02-11 19:17:18 +0100

    popup now shows previously uploaded imgs

--- extension/lib/popup.js
+++ extension/lib/popup.js
@@ -1,1 +1,32 @@
-console.log('popup');
+const storage = require('./storage');
+const app = document.getElementById('app');
+
+const viewUpload = u => {
+  const el = document.createElement('a');
+  const thumbnail = document.createElement('img');
+  const title = document.createElement('div');
+
+  el.classList.add('media');
+  el.classList.add('upload');
+  el.classList.add('px2');
+  thumbnail.classList.add('media-figure');
+  title.classList.add('media-body');
+  title.classList.add('truncate');
+
+  el.href = u.link;
+  el.alt = `Open image ${u.id} on imgur`;
+  thumbnail.src = u.thumbnail;
+  title.innerHTML = u.id;
+
+  el.appendChild(thumbnail);
+  el.appendChild(title);
+
+  return el;
+};
+
+const render = uv => app.appendChild(uv);
+
+storage.get().then(uploads => {
+  app.innerHTML = '';
+  uploads.map(viewUpload).forEach(render);
+});
